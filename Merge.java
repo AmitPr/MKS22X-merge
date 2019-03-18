f48ef9980d51dce024e86b4738ff5b0c38a5b01e
AmitPr <17amitprasad@gmail.com>
2019-03-18 19:10:58 -0400

    finished merge optimized for space, starting cutoff to insertion

--- Merge.java
+++ Merge.java
@@ -1,43 +1,72 @@
 import java.util.Arrays;
 
 public class Merge {
-    public static void main(String[] args) {
-        System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
-        int[] MAX_LIST = { 1000000000, 500, 10 };
-        for (int MAX : MAX_LIST) {
-            for (int size = 31250; size < 2000001; size *= 2) {
-                long qtime = 0;
-                long btime = 0;
-                // average of 5 sorts.
-                for (int trial = 0; trial <= 5; trial++) {
-                    int[] data1 = new int[size];
-                    int[] data2 = new int[size];
-                    for (int i = 0; i < data1.length; i++) {
-                        data1[i] = (int) (Math.random() * MAX);
-                        data2[i] = data1[i];
-                    }
-                    long t1, t2;
-                    t1 = System.currentTimeMillis();
-                    Merge.mergesort(data2);
-                    t2 = System.currentTimeMillis();
-                    qtime += t2 - t1;
-                    t1 = System.currentTimeMillis();
-                    Arrays.sort(data1);
-                    t2 = System.currentTimeMillis();
-                    btime += t2 - t1;
-                    if (!Arrays.equals(data1, data2)) {
-                        System.out.println("FAIL TO SORT!");
-                        System.exit(0);
-                    }
-                }
-                System.out.println(size + "\t\t" + MAX + "\t" + 1.0 * qtime / btime);
-            }
-            System.out.println();
-        }
-    }
+    static final int TRIALS = 5000;
+    //How many elements in array till it goes to insertion
+    static final int INSERTION = 7;
+	public static void main(String[] args) {
+		System.out.println("Size\t\tPerformance");
+
+		long stime, qtime, btime;
+		float ratio_sum = 0;
+
+		for (int size = 31250; size < 1000001; size <<= 1) {
+			qtime = btime = 0;
+
+			for (int t = 0; t < TRIALS; t++) {
+
+				int[] data1 = new int[size];
+				int[] data2 = new int[size];
+
+				for(int i = 0; i < data1.length; i++){
+					data2[i] = data1[i] = (int)(Math.random()*Integer.MAX_VALUE);
+				}
+
+				stime = System.currentTimeMillis();
+				Merge.mergesort(data2);
+				qtime += System.currentTimeMillis()-stime;
+
+				stime = System.currentTimeMillis();
+				Arrays.sort(data1);
+				btime += System.currentTimeMillis()-stime;
+
+				if (!Arrays.equals(data1, data2)) {
+					System.out.println("FAIL TO SORT!");
+					System.exit(0);
+				}
+			}
+			ratio_sum += 1.0*qtime/btime;
+			System.out.printf("%d\t\t%.3f\n", size, 1.0*qtime/btime);
+		}
+		System.out.printf("\nAverage\t\t%.3f\n", ratio_sum/6);
+	}
 
     public static void mergesort(int[] data) {
-        mergeHelper(data);
+        mergeHelper(data, Arrays.copyOf(data, data.length), 0, data.length);
+    }
+
+    private static void mergeHelper(int[] data, int[] temp, int lo, int hi) {
+        if(hi - lo <= CUTOFF){
+            
+        }
+        if (hi - lo > 1) {
+            //System.out.println(Arrays.toString(Arrays.copyOfRange(data, lo, hi)));
+            int mid = ((hi - lo) / 2) + lo;
+            mergeHelper(temp, data, lo, mid);
+            mergeHelper(temp, data, mid, hi);
+            merge(data, temp, lo, mid, hi);
+        }
+        //if sub-array is 2 long swap them
+        else if(hi - lo == 2){
+            if(temp[lo]>temp[lo+1]){
+                data[lo]=temp[lo+1];
+                data[lo+1]=temp[lo];
+            }else{
+                data[lo]=temp[lo];
+                data[lo+1]=temp[lo+1];
+            }
+        }
+        return;
     }
 
     private static void mergeHelper(int[] data) {
@@ -47,7 +76,6 @@ public class Merge {
             int[] right = Arrays.copyOfRange(data, data.length / 2, data.length);
             mergeHelper(right);
             merge(left, right, data);
-            // System.out.println(Arrays.toString(left) + " " + Arrays.toString(right));
         }
         return;
     }
@@ -57,7 +85,7 @@ public class Merge {
         j = k = 0;
         for (int i = 0; i < data.length; i++) {
             if (j < a1.length && k < a2.length) {
-                if (a1[j] < +a2[k]) {
+                if (a1[j] < a2[k]) {
                     data[i] = a1[j];
                     j++;
                 } else {
@@ -72,6 +100,31 @@ public class Merge {
                     data[i] = a1[j];
                     j++;
                 }
+            }
+        }
+    }
+
+    private static void merge(int[] data, int[] temp, int lo, int mid, int hi) {
+        int j, k;
+        j = lo;
+        k = mid;
+        for (int i = lo; i < hi; i++) {
+            if (j < mid) {
+                if (k < hi) {
+                    if (temp[j] < temp[k]) {
+                        data[i] = temp[j];
+                        j++;
+                    } else {
+                        data[i] = temp[k];
+                        k++;
+                    }
+                } else {
+                    data[i] = temp[j];
+                    j++;
+                }
+            } else {
+                data[i] = temp[k];
+                k++;
             }
         }
     }
