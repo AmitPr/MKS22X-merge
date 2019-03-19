fc7b1a75a0a179cc8f4b5fecaf2f9fa6a26fa2a8
AmitPr <17amitprasad@gmail.com>
2019-03-19 10:19:00 -0400

    Cutoff to insertion

--- Merge.java
+++ Merge.java
@@ -1,72 +1,83 @@
 import java.util.Arrays;
 
 public class Merge {
-    static final int TRIALS = 5000;
-    //How many elements in array till it goes to insertion
+    static final int TRIALS = 50;
+    // How many elements in array till it goes to insertion
     static final int INSERTION = 7;
-	public static void main(String[] args) {
-		System.out.println("Size\t\tPerformance");
 
-		long stime, qtime, btime;
-		float ratio_sum = 0;
+    public static void main(String[] args) {
+        System.out.println("Size\t\tPerformance");
 
-		for (int size = 31250; size < 1000001; size <<= 1) {
-			qtime = btime = 0;
+        long stime, qtime, btime;
+        float ratio_sum = 0;
 
-			for (int t = 0; t < TRIALS; t++) {
+        for (int size = 31250; size < 1000001; size <<= 1) {
+            qtime = btime = 0;
 
-				int[] data1 = new int[size];
-				int[] data2 = new int[size];
+            for (int t = 0; t < TRIALS; t++) {
 
-				for(int i = 0; i < data1.length; i++){
-					data2[i] = data1[i] = (int)(Math.random()*Integer.MAX_VALUE);
-				}
+                int[] data1 = new int[size];
+                int[] data2 = new int[size];
 
-				stime = System.currentTimeMillis();
-				Merge.mergesort(data2);
-				qtime += System.currentTimeMillis()-stime;
+                for (int i = 0; i < data1.length; i++) {
+                    data2[i] = data1[i] = (int) (Math.random() * Integer.MAX_VALUE);
+                }
 
-				stime = System.currentTimeMillis();
-				Arrays.sort(data1);
-				btime += System.currentTimeMillis()-stime;
+                stime = System.currentTimeMillis();
+                Merge.mergesort(data2);
+                qtime += System.currentTimeMillis() - stime;
 
-				if (!Arrays.equals(data1, data2)) {
-					System.out.println("FAIL TO SORT!");
-					System.exit(0);
-				}
-			}
-			ratio_sum += 1.0*qtime/btime;
-			System.out.printf("%d\t\t%.3f\n", size, 1.0*qtime/btime);
-		}
-		System.out.printf("\nAverage\t\t%.3f\n", ratio_sum/6);
-	}
+                stime = System.currentTimeMillis();
+                Arrays.sort(data1);
+                btime += System.currentTimeMillis() - stime;
+
+                if (!Arrays.equals(data1, data2)) {
+                    System.out.println("FAIL TO SORT!");
+                    System.exit(0);
+                }
+            }
+            ratio_sum += 1.0 * qtime / btime;
+            System.out.printf("%d\t\t%.3f\n", size, 1.0 * qtime / btime);
+        }
+        System.out.printf("\nAverage\t\t%.3f\n", ratio_sum / 6);
+    }
 
     public static void mergesort(int[] data) {
         mergeHelper(data, Arrays.copyOf(data, data.length), 0, data.length);
     }
 
     private static void mergeHelper(int[] data, int[] temp, int lo, int hi) {
-        if(hi - lo <= CUTOFF){
-            
-        }
-        if (hi - lo > 1) {
-            //System.out.println(Arrays.toString(Arrays.copyOfRange(data, lo, hi)));
-            int mid = ((hi - lo) / 2) + lo;
-            mergeHelper(temp, data, lo, mid);
-            mergeHelper(temp, data, mid, hi);
-            merge(data, temp, lo, mid, hi);
-        }
-        //if sub-array is 2 long swap them
-        else if(hi - lo == 2){
-            if(temp[lo]>temp[lo+1]){
-                data[lo]=temp[lo+1];
-                data[lo+1]=temp[lo];
-            }else{
-                data[lo]=temp[lo];
-                data[lo+1]=temp[lo+1];
+        if (hi - lo <= INSERTION) {
+            insertionSort(data, lo, hi);
+        } else {
+            if (hi - lo > 1) {
+                // System.out.println(Arrays.toString(Arrays.copyOfRange(data, lo, hi)));
+                int mid = ((hi - lo) / 2) + lo;
+                mergeHelper(temp, data, lo, mid);
+                mergeHelper(temp, data, mid, hi);
+                merge(data, temp, lo, mid, hi);
             }
         }
+        // if sub-array is 2 long swap them
+        /*
+         * else if(hi - lo == 2){ if(temp[lo]>temp[lo+1]){ data[lo]=temp[lo+1];
+         * data[lo+1]=temp[lo]; }else{ data[lo]=temp[lo]; data[lo+1]=temp[lo+1]; } }
+         */
         return;
+    }
+
+    private static void insertionSort(int[] data, int lo, int hi) {
+        for (int i = lo; i < hi; i++) {
+            for (int x = i; x > lo; x--) {
+                if (data[x] < data[x - 1]) {
+                    int temp = data[x];
+                    data[x] = data[x - 1];
+                    data[x - 1] = temp;
+                } else {
+                    break;
+                }
+            }
+        }
     }
 
     private static void mergeHelper(int[] data) {
